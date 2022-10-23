af42a8300bfdb46bc06a332b3cc980144c504f29
khersinpro <khersinpro@gmail.com>
2022-10-23 10:51:30 +0200

    add create article logic on json file

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# todo_php
 # forum-php
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,8 @@
 <header>
-    <div class="logo">Dyma Todo</div>
+    <a href="/" class="logo">Dyma Blog</a href="/">
+    <ul class="header-menu">
+        <li class="<?= $_SERVER["REQUEST_URI"] === '/add-article.php' ? "active" : "" ?>">
+            <a href="/add-article.php">Écrire un article</a>
+        </li>
+    </ul>
 </header>
